21913e44489083f1f16dc4d1b606490de20107c8
Bishwajit-Bhattacharjee <bishwajit0020@gmail.com>
2022-10-07 13:48:28 +0600

    update script

--- src/kvraft/script.sh
+++ src/kvraft/script.sh
@@ -1,7 +1,8 @@
-tasks="TestManyPartitionsOneClient3A"
+# tasks="TestManyPartitionsOneClient3A"
 tasks="TestSpeed3A TestPersistPartitionUnreliableLinearizable3A"
+# tasks="TestSpeed3B"
 #
-task_no="3[A]"
-# tasks=$(grep "$task_no(t" test_test.go | cut -d "(" -f 1 | cut -d " " -f 2 | xargs)
+task_no="3[A|B]"
+tasks=$(grep "$task_no(t" test_test.go | cut -d "(" -f 1 | cut -d " " -f 2 | xargs)
 ./dstest.py $tasks -p 9 -n $1
 
